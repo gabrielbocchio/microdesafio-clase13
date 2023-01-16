2b8f93ea448905e6c73d0cbb770904863a1ec197
gabocchio <bocchio.gabriel@gmail.com>
2023-01-16 15:33:33 +0100

    acepté tus modificaciones

--- object_pato.js
+++ object_pato.js
@@ -1,10 +1,3 @@
 // PRUEBA NRO 1
-<<<<<<< HEAD
-
-//estoy probando una linea de texto porque tengo que escribir texto
-
-console.log("2");
-=======
 // Gabo deja de complicar los ejercicos.
 // DALE GABOOOOO!!!
->>>>>>> 1e0d6dfb9fc63905473f2310203020888c033e29
